9a072eaecc290482decd61df9426bcb2988b91e1
dhd <2979077670@qq.com>
2019-09-06 09:02:17 +0800

    9.6日考

---
+++ 9.6/myself/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <div class="content">
+      <router-view/>
+    </div>
+    <div class="bottom">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+html,body,#app{
+  width: 100%;
+  height: 100%;
+}
+#app{
+  display: flex;
+  flex-direction: column;
+}
+.content{
+  width: 100%;
+  height: 100%;
+}
+.bottom{
+  width: 100%;
+  height: 30px;
+  background: #ccc;
+  display: flex;
+  a{
+    width: 50%;
+    height: 100%;
+    text-decoration: none;
+    color: #333;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
---
+++ 9.6/myself/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <div class="header">多加网</div>
+    <div class="main">
+      <swiper>
+        <swiper-slide><img src="../assets/logo.png" alt=""></swiper-slide>
+        <swiper-slide><img src="../assets/logo.png" alt=""></swiper-slide>
+        <swiper-slide><img src="../assets/logo.png" alt=""></swiper-slide>
+      </swiper>
+      <div v-for="(item,index) in list" :key="index">{{item.name}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+  data() {
+    return {
+      list:[]
+    }
+  },
+  name: "home",
+  components: {
+
+  },
+  created() {
+      axios.get("/list").then(res=>{
+          this.list=res.data.data
+      })
+  },
+};
+</script>
